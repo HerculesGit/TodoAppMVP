b2fe9233caf6ec3948179461fef512fc0ca06314
Hercules Silva de Souza <hercules.silva@dcx.ufpb.br>
2021-05-27 09:52:20 -0300

    first commit

---
+++ app/src/test/java/br/com/herco/todoappmvp/EditTaskPresenterTest.java
@@ -0,0 +1,87 @@
+package br.com.herco.todoappmvp;
+
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import br.com.herco.todoappmvp.edit_task.EditTaskPresenter;
+import br.com.herco.todoappmvp.edit_task.IEditTaskContract;
+import br.com.herco.todoappmvp.exceptions.TaskException;
+import br.com.herco.todoappmvp.models.TaskModel;
+import br.com.herco.todoappmvp.repositories.task.TaskRepository;
+import br.com.herco.todoappmvp.services.database.preferences.DataBasePreferences;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertThrows;
+import static org.mockito.Mockito.when;
+
+@RunWith(MockitoJUnitRunner.class)
+public class EditTaskPresenterTest {
+
+    private EditTaskPresenter presenter;
+
+    @Mock
+    private TaskRepository repository;
+
+    @Mock
+    private IEditTaskContract viewContract;
+
+    @Before
+    public void setUp() throws Exception {
+        MockitoAnnotations.initMocks(this);
+
+        presenter = Mockito.spy(new EditTaskPresenter(viewContract, repository));
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void test1() throws Throwable {
+        assertFalse(throwException());
+    }
+
+    private boolean throwException() {
+        throw new IllegalArgumentException();
+    }
+
+    @Test()
+    public void shouldThrowExceptionToAddingANullTask() throws TaskException {
+        when(repository.createTask(null)).thenThrow(new TaskException("Task cannot null!"));
+
+        // really verify if the method throws TaskException
+        Exception exception = assertThrows(TaskException.class, () -> repository.createTask(null));
+
+        assertEquals(exception.getMessage(), "Task cannot null!");
+    }
+
+    @Test()
+    public void shouldThrowsExceptionToAddANullTaskName() throws TaskException {
+        final TaskModel taskModel = new TaskModel(null);
+
+        when(repository.createTask(taskModel)).thenThrow(new TaskException("Task's name cannot null!"));
+
+        Exception exception = assertThrows(TaskException.class, () -> repository.createTask(taskModel));
+        assertEquals(exception.getMessage(), "Task's name cannot null!");
+    }
+
+    @Test()
+    public void shouldAddANameToTheTask() throws TaskException {
+        final String taskName = "Learn unit test to android with java";
+        final TaskModel taskModel = new TaskModel(taskName);
+
+        when(repository.createTask(taskModel)).thenReturn(taskModel);
+
+        assertNotNull(taskModel.getName());
+        assertEquals(taskModel.getName(), taskName);
+    }
+
+//    @Test()
+//    public void shouldAddIdToTheTask() throws TaskException {
+//    }
+
+}
---
+++ app/src/test/java/br/com/herco/todoappmvp/TaskValidatorTest.java
@@ -0,0 +1,79 @@
+package br.com.herco.todoappmvp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import br.com.herco.todoappmvp.exceptions.TaskException;
+import br.com.herco.todoappmvp.models.TaskModel;
+import br.com.herco.todoappmvp.validators.TaskValidator;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+@RunWith(MockitoJUnitRunner.class)
+public class TaskValidatorTest {
+
+    @Test(expected = TaskException.class)
+    public void shouldThrowsTaskExceptionToValidateANullTaskBeforeCreate() throws TaskException {
+        TaskModel taskModel = null;
+        TaskValidator.validateTaskToCreate(taskModel);
+    }
+
+    @Test(expected = TaskException.class)
+    public void shouldThrowsTaskExceptionToValidateANullTaskNameBeforeCreate() throws TaskException {
+        TaskModel taskModel = new TaskModel(null);
+        TaskValidator.validateTaskToCreate(taskModel);
+    }
+
+    @Test()
+    public void shouldValidateToANotNullTaskBeforeCreate() throws TaskException {
+        final String taskName = "Learn unit test with Java";
+        TaskModel taskModel = new TaskModel(taskName);
+        TaskValidator.validateTaskToCreate(taskModel);
+
+        assertNotNull(taskModel);
+    }
+
+    @Test()
+    public void shouldValidateANotNullTaskNameBeforeCreate() throws TaskException {
+        final String taskName = "Learn unit test with Java";
+        TaskModel taskModel = new TaskModel(taskName);
+        TaskValidator.validateTaskToCreate(taskModel);
+
+        assertEquals(taskModel.getName(), taskName);
+    }
+
+
+    // = UPDATE =
+    @Test(expected = TaskException.class)
+    public void shouldThrowsTaskExceptionToValidateANullTaskBeforeUpdate() throws TaskException {
+        TaskModel taskModel = null;
+        TaskValidator.validateTaskToUpdate(taskModel);
+    }
+
+    @Test(expected = TaskException.class)
+    public void shouldThrowsTaskExceptionToValidateANullTaskNameBeforeUpdate() throws TaskException {
+        TaskModel taskModel = new TaskModel(null);
+        TaskValidator.validateTaskToUpdate(taskModel);
+    }
+
+    @Test(expected = TaskException.class)
+    public void shouldThrowsTaskExceptionToValidateANullTaskIdBeforeUpdate() throws TaskException {
+        final String taskName = "Learn unit test with Java";
+        TaskModel taskModel = new TaskModel(taskName);
+
+        TaskValidator.validateTaskToUpdate(taskModel);
+    }
+
+    @Test()
+    public void shouldValidateToANotNullTaskBeforeUpdate() throws TaskException {
+        final String taskName = "Learn unit test with Java";
+        TaskModel taskModel = new TaskModel(taskName);
+
+        taskModel.setId(1);
+        TaskValidator.validateTaskToUpdate(taskModel);
+
+        assertNotNull(taskModel);
+    }
+}
